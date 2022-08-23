17dc5d58b67d2c660451a47e450e40d23bffad44
nikita tarasov <nikita@mygento.ru>
2022-08-23 16:43:35 +0300

    2.4.0

--- Plugin/Resolver.php
+++ Plugin/Resolver.php
@@ -14,7 +14,7 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 
 class Resolver
 {
-    const BLOCKED_LIST = ['cart', 'wishlist'];
+    public const BLOCKED_LIST = ['cart', 'wishlist'];
 
     /**
      * @var \Magento\Framework\Serialize\SerializerInterface
